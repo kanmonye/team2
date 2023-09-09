5715198d8b2a0aa828572c92c60093b5aa9506ea
kanmonye <58056024+kanmonye@users.noreply.github.com>
2023-09-09 11:58:08 -0500

    Update main.tf

--- team2/main.tf
+++ team2/main.tf
@@ -46,3 +46,15 @@ resource "aws_security_group" "my-new-security-group" {
 resource "random_id" "randomness" {
   byte_length = 16
 }
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.250.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name     = "sub-variables-us-east-1a"
+    Terraform = "true"
+  }
+}
