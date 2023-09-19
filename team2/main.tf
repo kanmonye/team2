4adfdb403aa03f6813693a19d20fb87178d5b2a8
kanmonye <58056024+kanmonye@users.noreply.github.com>
2023-09-19 16:19:15 -0500

    Update main.tf

--- team2/main.tf
+++ team2/main.tf
@@ -85,3 +85,15 @@ create_lt = true
 tags_as_map = {
     Name = "Web EC2 Server 2"
   }
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
+output "public_dns" {
+  value = aws_instance.web.public_dns
+}
+
+output "size" {
+description = "Size of server built with Server Module"
+value = aws_instance.web.instance_type
+}
